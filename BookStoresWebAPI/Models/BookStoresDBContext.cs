6eaebffdf9e6fbfc0833ff36a38e83522a8813f6
SachinthaWeerasena <77657271+SachinthaWeerasena@users.noreply.github.com>
2021-01-25 10:38:59 +0530

    Add files via upload

---
+++ BookStoresWebAPI/Models/BookStoresDBContext.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace BookStoresWebAPI.Models
+{
+    public class BookStoresDBContext : DbContext
+    {
+        public DbSet<Publisher> Publishers{get; set;}
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Data Source=.\sqlexpress;Initial Catalog=BookStoresDB;Integrated Security=True");
+        }
+    }
+}
